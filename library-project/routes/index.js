f5feae79acd6a6b8ce78b658c150f11443cfb587
Rémy Lenoir <remylenoir.dev@tuta.io>
2019-04-15 14:48:33 +0200

    Update books

--- library-project/routes/index.js
+++ library-project/routes/index.js
@@ -8,30 +8,51 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/books", (req, res) => {
-  // get all the books
   Book.find({})
     .then(books => {
       res.render("books", { books });
     })
     .catch(err => {
-      console.error(err);
+      console.error("Error while finding the books", err);
     });
-  //
 });
 
-router.get("/books/add", (req, res) => {
-  res.render("book-add");
+router.post("/books/edit/:bookId", (req, res) => {
+  const { title, author, description, rating } = req.body;
+  const _id = req.params.bookId;
+  Book.findOneAndUpdate({ _id }, { title, author, description, rating }, { new: true })
+    .then(updatedData => {
+      res.redirect(`/books/${updatedData._id}`);
+    })
+    .catch(err => {
+      console.error("Error while updating the book in the database", err);
+    });
 });
 
 router.post("/books/add", (req, res) => {
   const { title, author, description, rating } = req.body;
   Book.create({ title, author, description, rating })
     .then(() => {
+      console.log("Book successfully created");
       res.redirect("/books");
-      console.log("Book successfully created.");
     })
     .catch(err => {
-      console.error("Error while creating the book", err);
+      console.error("Error while creating book", err);
+    });
+});
+
+router.get("/books/add", (req, res) => {
+  res.render("book-add");
+});
+
+router.get("/books/edit/:bookId", (req, res) => {
+  const _id = req.params.bookId;
+  Book.findOne({ _id })
+    .then(book => {
+      res.render("book-edit", { book });
+    })
+    .catch(err => {
+      console.error("Error while getting book", err);
     });
 });
 
@@ -42,7 +63,7 @@ router.get("/books/:bookId", (req, res) => {
       res.render("book-detail", { book });
     })
     .catch(err => {
-      console.error(err);
+      console.error("Error while retrieving book with id " + _id, err);
     });
 });
 
